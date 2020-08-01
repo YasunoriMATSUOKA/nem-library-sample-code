7030e97fd4c84f0a948c87ef1323622bc6a927e9
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2020-08-01 15:33:30 +0900

    ノードプールを用いた、より堅牢な、アカウント情報取得のサンプルコード追加

---
+++ src/2/getAccountInfoWithMetaDataFromAddress.ts
@@ -0,0 +1,25 @@
+/*
+2. 残高情報の取得と表示(1と同様の処理だが、単一ノードでなく複数ノードを扱える実装で、より堅牢)
+ノード, アドレスを指定し、REST-APIリクエストし、残高等の情報を取得し、表示するサンプルコード
+*/
+import { NEMLibrary, NetworkTypes, AccountHttp, Address } from "nem-library";
+
+// 他の箇所でも何度も使用するので、一か所でまとめて定義してexportしたものをimportして使用
+import { serverConfigs } from "../configs/nodes";
+
+// 以降は1と同様
+NEMLibrary.bootstrap(NetworkTypes.MAIN_NET);
+
+const accountHttp = new AccountHttp(serverConfigs);
+const address = new Address("NAX3EP-4EKUML-H4T3N7-GJSTQD-G2AMUK-5T26P5-EA2M");
+accountHttp.getFromAddress(address).subscribe(
+  (x) => {
+    console.log(x);
+  },
+  (error) => {
+    console.log(error);
+  },
+  () => {
+    console.log("Completed!");
+  }
+);
---
+++ src/configs/nodes.ts
@@ -0,0 +1,113 @@
+import { ServerConfig } from "nem-library";
+
+const httpsProtocol = "https";
+const httpsPort = 7891;
+
+// 以下では、https://github.com/ethersecurity/nodes/blob/master/nem/nodes.txt のノードを使わせて頂いています。ありがとうございます。
+export const serverConfigs: ServerConfig[] = [
+  {
+    protocol: httpsProtocol,
+    domain: "aqualife1.supernode.me",
+    port: httpsPort,
+  },
+  {
+    protocol: httpsProtocol,
+    domain: "aqualife2.supernode.me",
+    port: httpsPort,
+  },
+  {
+    protocol: httpsProtocol,
+    domain: "aqualife3.supernode.me",
+    port: httpsPort,
+  },
+  {
+    protocol: httpsProtocol,
+    domain: "beny.supernode.me",
+    port: httpsPort,
+  },
+  {
+    protocol: httpsProtocol,
+    domain: "mnbhsgwbeta.supernode.me",
+    port: httpsPort,
+  },
+  {
+    protocol: httpsProtocol,
+    domain: "mnbhsgwgamma.supernode.me",
+    port: httpsPort,
+  },
+  {
+    protocol: httpsProtocol,
+    domain: "nemstrunk.supernode.me",
+    port: httpsPort,
+  },
+  {
+    protocol: httpsProtocol,
+    domain: "nemstrunk2.supernode.me",
+    port: httpsPort,
+  },
+  {
+    protocol: httpsProtocol,
+    domain: "mttsukuba.supernode.me",
+    port: httpsPort,
+  },
+  {
+    protocol: httpsProtocol,
+    domain: "pegatennnag.supernode.me",
+    port: httpsPort,
+  },
+  {
+    protocol: httpsProtocol,
+    domain: "shibuya.supernode.me",
+    port: httpsPort,
+  },
+  {
+    protocol: httpsProtocol,
+    domain: "thomas1.supernode.me",
+    port: httpsPort,
+  },
+  {
+    protocol: httpsProtocol,
+    domain: "xemcat.supernode.me",
+    port: httpsPort,
+  },
+  {
+    protocol: httpsProtocol,
+    domain: "snnode.supernode.me",
+    port: httpsPort,
+  },
+  {
+    protocol: httpsProtocol,
+    domain: "nemlovely1.supernode.me",
+    port: httpsPort,
+  },
+  {
+    protocol: httpsProtocol,
+    domain: "nemlovely2.supernode.me",
+    port: httpsPort,
+  },
+  {
+    protocol: httpsProtocol,
+    domain: "nemlovely3.supernode.me",
+    port: httpsPort,
+  },
+  {
+    protocol: httpsProtocol,
+    domain: "nemlovely4.supernode.me",
+    port: httpsPort,
+  },
+  {
+    protocol: httpsProtocol,
+    domain: "nemlovely5.supernode.me",
+    port: httpsPort,
+  },
+  {
+    protocol: httpsProtocol,
+    domain: "nemlovely6.supernode.me",
+    port: httpsPort,
+  },
+  {
+    protocol: httpsProtocol,
+    domain: "nemlovely7.supernode.me",
+    port: httpsPort,
+  },
+];
